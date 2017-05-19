0165f4bac216de4ee740cd2dadff8c88dc7933e9
0E800 <0ld3nglish800@gmail.com>
2017-05-19 23:52:25 +0000

    modified:   less/extension.less 	modified:   src/Listeners/AddBBCode.php

--- src/Listeners/AddBBCode.php
+++ src/Listeners/AddBBCode.php
@@ -91,6 +91,110 @@ $event->configurator->BBCodes->addCustom(
 
 
 
+$event->configurator->BBCodes->addCustom(
+ '[roboto]{TEXT}[/roboto]',
+            '<p class="roboto">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[robotoslab]{TEXT}[/robotoslab]',
+            '<p class="robotoslab">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[khula]{TEXT}[/khula]',
+            '<p class="khula">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[barrio]{TEXT}[/barrio]',
+            '<p class="barrio">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[montserratsubrayada]{TEXT}[/montserratsubrayada]',
+            '<p class="montserratsubrayada">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[raleway]{TEXT}[/raleway]',
+            '<p class="raleway">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[raviprakash]{TEXT}[/raviprakash]',
+            '<p class="raviprakash">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[lora]{TEXT}[/lora]',
+            '<p class="lora">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[droidsans]{TEXT}[/droidsans]',
+            '<p class="droidsans">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[ubuntu]{TEXT}[/ubuntu]',
+            '<p class="ubuntu">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[droidserif]{TEXT}[/droidserif]',
+            '<p class="droidserif">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[baloo]{TEXT}[/baloo]',
+            '<p class="baloo">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[ptserif]{TEXT}[/ptserif]',
+            '<p class="ptserif">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[indieflower]{TEXT}[/indieflower]',
+            '<p class="indieflower">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[dosis]{TEXT}[/dosis]',
+            '<p class="dosis">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[oxygen]{TEXT}[/oxygen]',
+            '<p class="oxygen">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[anton]{TEXT}[/anton]',
+            '<p class="anton">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[cabin]{TEXT}[/cabin]',
+            '<p class="cabin">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[arvo]{TEXT}[/arvo]',
+            '<p class="arvo">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[lobster]{TEXT}[/lobster]',
+            '<p class="lobster">{TEXT}</p>'
+        );
+
+$event->configurator->BBCodes->addCustom(
+ '[pacifico]{TEXT}[/pacifico]',
+            '<p class="pacifico">{TEXT}</p>'
+        );
 
 
     }
